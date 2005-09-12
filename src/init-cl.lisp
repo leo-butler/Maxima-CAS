435aab7afcfe1a9c215925f0bf470966c97918c6
riotorto <riotorto>
2005-09-12 14:46:32 +0000

    Adding contrib/descriptive and contrib/distrib to variable share-subdirs

--- src/init-cl.lisp
+++ src/init-cl.lisp
@@ -205,7 +205,7 @@
 	 (maxima-patterns "###.{mac,mc}")
 	 (demo-patterns "###.{dem,dm1,dm2,dm3,dmt}")
 	 (usage-patterns "##.{usg,texi}")
-	 (share-subdirs "{affine,algebra,calculus,combinatorics,contrib,contrib/nset,contrib/pdiff,diffequations,graphics,integequations,integration,macro,matrix,misc,numeric,physics,simplification,specfunctions,sym,tensor,trigonometry,utils,vector}"))
+	 (share-subdirs "{affine,algebra,calculus,combinatorics,contrib,contrib/nset,contrib/pdiff,contrib/numericalio,contrib/descriptive,contrib/distrib,linearalgebra,diffequations,graphics,integequations,integration,macro,matrix,misc,numeric,physics,simplification,specfunctions,sym,tensor,trigonometry,utils,vector}"))
     (setq $file_search_lisp
 	  (list '(mlist)
 		;; actually, this entry is not correct.
@@ -419,7 +419,7 @@
 	    (process-maxima-args input-stream batch-flag))
       (progn
 	(loop 
-	 (with-simple-restart (macsyma-quit "Macsyma top-level")
+	 (with-simple-restart (macsyma-quit "Maxima top-level")
 	     (macsyma-top-level input-stream batch-flag)))))))
 
 (import 'cl-user::run)
