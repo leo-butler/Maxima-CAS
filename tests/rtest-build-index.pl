25b5a9b2a17e9a89c2708a976696161a92deb09e
Leo Butler <nb0yjxtr+github@sdf.lonestar.org>
2011-03-04 22:05:51 +0000

    a re-write of rtest-build-index.sh in perl, because I don't want to fight with the shell's escape/quote machinery

---
+++ tests/rtest-build-index.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/perl -s
+use warnings;
+use strict;
+use File::Find;
+sub find_info_subdirs(@);
+sub set_encoding($);
+sub vprint(@);
+sub msystem(@);
+
+my $pid=$$;
+our $verbose||=undef;
+our $info_dir||="$ENV{PWD}/../doc/info/";
+our $info_subdirs||=undef;
+our @info_subdirs=find_info_subdirs($info_dir, 'maxima.info', $info_subdirs); #$(find $info_dir -name 'maxima.info' -execdir pwd \;);
+our $xterm||='xterm';
+our $xterm_opts||='-exec';
+our $xterm_cmd||="$xterm $xterm_opts";
+our $lisps||="sbcl:clisp:cmucl";
+our @lisps=split(/:/,$lisps);
+our $maxima_opts||='--init=/dev/null --very-quiet';
+our $maxima_cmd||="$ENV{PWD}/../maxima-local";
+our $maxima_batch_string||='describe("expand");system("printenv;");read("quit?");';
+our $run_rtest_build_index||=1;
+our $rtest_build_index_bs||='load("rtest-run.lisp");';
+our @locale=grep {/^LC_|LOCALE/ } keys %ENV;
+
+if ($run_rtest_build_index == 1) {
+   for my $lisp (@lisps) {
+      msystem("$maxima_cmd $maxima_opts -l $lisp --batch-string='$rtest_build_index_bs'");
+   }
+}
+for my $dir (@info_subdirs) {
+   my %locale_settings=set_encoding($dir);
+   vprint "Testing in $dir";
+   for my $lisp (@lisps) {
+      open my $shell, "|/bin/bash";
+      print $shell "cd $dir;";
+      foreach my $e (keys %locale_settings) {
+	 my $v=$locale_settings{$e};
+	 print $shell "export $e=$v;";
+      }
+      vprint "$xterm_cmd $maxima_cmd $maxima_opts -l $lisp --batch-string='$maxima_batch_string';";
+      print $shell "$xterm_cmd $maxima_cmd $maxima_opts -l $lisp --batch-string='$maxima_batch_string'";
+   }
+}
+
+sub find_info_subdirs(@)
+{
+   my ($infodir,$maxima_info,$info_subdirs)=@_;
+   if ($info_subdirs) {
+      return split(/:/,$info_subdirs);
+   }
+   my %infodirs;
+   my $wanted=sub {
+      /^$maxima_info/s && $infodirs{$File::Find::dir}++;
+   };
+   &File::Find::find({wanted => $wanted}, $infodir);
+   return keys %infodirs;
+}
+sub set_encoding($)
+{
+   my $dir=shift;
+   my @path=split(m{/},"$dir");
+   my ($lang,$code)=split(/\./,$path[-1]);
+   my %encoding;
+   if($lang eq "info"){ return (); };
+   $code=($code ? "UTF-8" : "ISO-8859-1");
+   if(length($lang)==2){$lang=~s/(.+)/$1_\U$1/;}
+   foreach (@locale) { $encoding{$_}="${lang}.${code}"; }
+   return %encoding;
+}
+sub vprint(@)
+{
+   local $,=" ";
+   $verbose and print @_,"\n";
+}
+sub msystem(@)
+{
+   vprint @_;
+   system(@_);
+}
