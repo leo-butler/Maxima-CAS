b813a39a3ed1a71a18e256ad353eaa54710af0e0
Leo Butler <nb0yjxtr+github@sdf.lonestar.org>
2011-03-05 23:29:43 +0000

    removed scheme files+bugs, moved test file from scheme to lisp

--- src/pregexp/dialects/cl-pregexp.scm
+++
@@ -1,9 +0,0 @@
-(scmxlate-rename-define
- (pregexp-reverse! #'nreverse)
- )
-
-(scmxlate-postprocess
- (if (probe-file "pregexp.cl") (delete-file "pregexp.cl"))
-
- (rename-file "my-pregexp.scm" "pregexp.cl")
- )
--- src/pregexp/dialects/files-to-be-ported.scm
+++
@@ -1,1 +0,0 @@
-pregexp.scm
